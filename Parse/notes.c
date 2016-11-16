19498cf5abf24be682ee4dd6be31b70e6b6983f9
gugwf9 <gugwf9@mail.missouri.edu>
2016-11-15 19:13:23 -0600

    Add files via upload
    adding a few files, nothing major

---
+++ Parse/notes.c
@@ -0,0 +1,9 @@
+print current line, which will be year, semester and local campus credits to file
+while(loop til next time you find "Local Campus Credits"){
+	if(getline returns string that is in contents of set file of courses){
+		write that course to new file\n
+	}
+}
+
+
+math, chem and stat all print out course with return 
